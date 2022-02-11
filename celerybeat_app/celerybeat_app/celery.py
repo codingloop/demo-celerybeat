2d76823a173d450b4ea9b504d878749d7579bb66
codingloop <ishunfs@gmail.com>
2022-02-11 11:07:28 +0530

    adding imports

--- celerybeat_app/celerybeat_app/celery.py
+++ celerybeat_app/celerybeat_app/celery.py
@@ -3,6 +3,6 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celerybeat_app.settings')
 
-app = Celery('celerybeat_app')
+app = Celery('celerybeat_app', include=['celerybeat_app.tasks'])
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
