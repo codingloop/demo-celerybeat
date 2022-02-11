769d736efb41c8668d5d27087b9e6d38b4a0a764
codingloop <ishunfs@gmail.com>
2022-02-11 11:38:17 +0530

    Update task

--- celerybeat_app/celerybeat_app/tasks.py
+++ celerybeat_app/celerybeat_app/tasks.py
@@ -5,5 +5,5 @@ from celerybeat_app.celery import app
 
 @app.task
 def make_txt():
-    with open(f"{randint}_file.txt", "w") as f:
+    with open(f"media/{randint(1, 1000)}_file.txt", "w") as f:
         pass
